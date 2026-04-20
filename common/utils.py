3ac8fccf9913b3f9a0fdc5504bbac5fcc015539b
Azadeh Hadadi <55028876+Azihadadi@users.noreply.github.com>
2026-04-20 21:24:21 +0200

    Add files via upload

--- common/utils.py
+++ common/utils.py
@@ -1,1 +1,57 @@
+from __future__ import print_function
+import sys
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    from functools import reduce
+
+import numpy as np
+import cv2
+
+# get and draw the init ground truth (ROI)..
+class RectSelector:
+    def __init__(self, win, callback):
+        self.win = win
+        self.callback = callback
+        cv2.setMouseCallback(win, self.onmouse)
+        self.drag_start = None
+        self.drag_rect = None
+
+    def onmouse(self, event, x, y, flags, param):
+        x, y = np.int16([x, y])
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drag_start = (x, y)
+            return
+        if self.drag_start:
+            if flags & cv2.EVENT_FLAG_LBUTTON:
+                xo, yo = self.drag_start
+                x0, y0 = np.minimum([xo, yo], [x, y])
+                x1, y1 = np.maximum([xo, yo], [x, y])
+                self.drag_rect = None
+                if x1 - x0 > 0 and y1 - y0 > 0:
+                    self.drag_rect = (x0, y0, x1, y1)
+            else:
+                rect = self.drag_rect
+                self.drag_start = None
+                self.drag_rect = None
+                if rect:
+                    self.callback(rect)
+
+    def draw(self, vis):
+        if not self.drag_rect:
+            return False
+        x0, y0, x1, y1 = self.drag_rect
+        cv2.rectangle(vis, (x0, y0), (x1, y1), (255, 0, 0), 2)
+        return True
+
+def draw_keypoints(vis, keypoints, color=(0, 255, 255)):
+    for kp in keypoints:
+        x, y = kp.pt
+        cv2.circle(vis, (int(x), int(y)), 2, color)
+
+def draw_str(dst, target, s):
+    x, y = target
+    cv2.putText(dst, s, (x + 1, y + 1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+    cv2.putText(dst, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
 
